051163f20aa88e8dc56ac5823027f2e58dc93b89
Bhargav Mekala <bmekala@dfi.dc>
2025-02-10 13:17:02 +0530

    first3

--- datafactz/settings.py
+++ datafactz/settings.py
@@ -249,7 +249,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.getenv('DATABASE_PATH', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'NAME': os.getenv('DATABASE_PATH', os.path.join(BASE_DIR, 'db.sqlite4')),
     }
 }
 
